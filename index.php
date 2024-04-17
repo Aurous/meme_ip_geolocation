210d550863d4c8ed45e1bc4b2af61e0a26fca4d1
Ryan Hill <ltryanhill@gmail.com>
2024-04-16 19:42:42 -0500

    better

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+
 // Import Reader
 require 'vendor/autoload.php';
 use GeoIp2\Database\Reader;
 // Init Reader
 $cityDbReader = new Reader('./GeoLite2-City.mmdb');
 
+// Ensure IP
+$ip = (!empty($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : (!empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR']));
+
 // Read IP
-$record = $cityDbReader->city($_SERVER['REMOTE_ADDR']);
+$record = $cityDbReader->city($ip);
 
 // File and new size
-$location = "In the " . $record->city->name . " area";
+$area = "In the " . (!empty($record->city->name) ? $record->city->name : "Unknown") . " area";
 $string = "Local girls looking for love";
 $filename = 'image.jpg';
 $percent = 0.4;
@@ -40,8 +41,8 @@ $top_px = (imagesx($thumb) - 9 * strlen($string)) / 2;
 imagestring($thumb, 5, $top_px, 9, $string, $black);
 
 // Add Bottom Text
-$bottom_px = (imagesx($thumb) - 9 * strlen($location)) / 2;
-imagestring($thumb, 5, $bottom_px, 254, $location, $black);
+$bottom_px = (imagesx($thumb) - 9 * strlen($area)) / 2;
+imagestring($thumb, 5, $bottom_px, 254, $area, $black);
 
 // Output
 imagejpeg($thumb);
